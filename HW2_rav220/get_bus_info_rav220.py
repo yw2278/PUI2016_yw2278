9323903456afe05851799050bd06e57e0996ac47
Richard Vecsler <rav220@compute.cusp.nyu.edu>
2016-09-19 13:24:37 -0400

    new changes

--- HW2_rav220/get_bus_info_rav220.py
+++ HW2_rav220/get_bus_info_rav220.py
@@ -5,9 +5,7 @@
 # Assignment 2
 # Python Script to scrape and parse MTA Bus data
 
-
 #####################
-
 
 from __future__ import print_function
 import sys
@@ -18,21 +16,18 @@ import csv
 # test number of inputs
 
 if not len(sys.argv) == 4:
-	print("Invalid number of arguments.  Run as python show_bus_locations.py <MTA_KEY> <BUS_LINE>")
-	sys.exit()
-
+        print("Invalid number of arguments.  Run as python show_bus_locations.py <MTA_KEY> <BUS_LINE>")
+        sys.exit()
 
 # scrape MTA site
 
 MTA_key = sys.argv[1]
 Bus_line = sys.argv[2]
 
-
 bus_json_url="http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + MTA_key + "&VehicleMonitoringDetailLevel=calls&LineRef=" + Bus_line
 response = urllib.urlopen(bus_json_url)
 bus_data = response.read().decode("utf-8")
 data = json.loads(bus_data)
-
 
 fout = csv.writer(open(sys.argv[3], "wb+"))
 
@@ -40,8 +35,20 @@ fout.writerow(['Latitude','Longitude','Stop Time','Stop Status'])
 
 
 for it in data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']:
-	fout.writerow([it['MonitoredVehicleJourney']['VehicleLocation']['Latitude'],
-	it['MonitoredVehicleJourney']['VehicleLocation']['Longitude'],
-	it['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName'],
-	it['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']])
 
+        if len(it['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']) == 0:
+                stop = 'N/A'
+
+        else:
+                stop = it['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+
+        if len(it['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']) == 0:
+                dist = 'N/A'
+        else:
+                dist = it['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+
+        lat = it['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+        lon = it['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+
+
+        fout.writerow([lat,lon,stop,dist])
