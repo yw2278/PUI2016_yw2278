c0738b09c60aea8e2b0ba3e09ffdc5d29fad96f0
Yao Wang <yw2278@compute.cusp.nyu.edu>
2016-10-10 21:31:23 -0400

    ...

--- HW5_yw2278/getCitiBikeCSV.py
+++ HW5_yw2278/getCitiBikeCSV.py
@@ -1,6 +1,6 @@
 import os
 def getCitiBikeCSV(datestring):
-    print "Downloading", datestring
+    print ("Downloading", datestring)
     ### First I will heck that it is not already there
     if not os.path.isfile(os.getenv("PUIDATA") + "/" + datestring + "-citibike-tripdata.csv"):
         if os.path.isfile(datestring + "-citibike-tripdata.csv"):
